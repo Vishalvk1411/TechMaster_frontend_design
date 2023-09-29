9f7993218cad380b149a5e9a580e2a5fe006417c
Vishal Virpakshi Kalshetty <139418188+Vishalvk1411@users.noreply.github.com>
2023-09-29 12:54:55 +0530

    Add files via upload

---
+++ Device.js
@@ -0,0 +1,128 @@
+
+function phones(){
+    document.getElementById("appleproducts").style.display="none"
+    document.getElementById("laptopcatagory").style.display="none"
+    document.getElementById("carouselExampleAutoplaying").style.display="none"
+    document.getElementById("card").style.display="none"
+    document.getElementById("phones0").style.display="block"
+    document.getElementById("yourlapptopyouruse").style.display="none"
+    document.getElementById("laptops").style.display="none"
+    var k=true
+    if(k=""){
+        return true
+    }
+    else{
+        return false
+    }
+
+}
+
+function phonesunder15k(){
+    document.getElementById("appleproducts").style.display="none"
+    document.getElementById("laptopcatagory").style.display="none"
+    document.getElementById("carouselExampleAutoplaying").style.display="none"
+    document.getElementById("card").style.display="none"
+    document.getElementById("phones0").style.display="none"
+    document.getElementById("yourlapptopyouruse").style.display="none"
+    document.getElementById("laptops").style.display="none"
+    document.getElementById("under15k").style.display="block"
+    document.getElementById("btw15k-20k").style.display="none"
+    document.getElementById("above20k").style.display="none"
+    var k=true
+    if(k=""){
+        return true
+    }
+    else{
+        return false
+    }
+
+}
+
+function phones15k20k(){
+    document.getElementById("appleproducts").style.display="none"
+    document.getElementById("laptopcatagory").style.display="none"
+    document.getElementById("carouselExampleAutoplaying").style.display="none"
+    document.getElementById("card").style.display="none"
+    document.getElementById("phones0").style.display="block"
+    document.getElementById("yourlapptopyouruse").style.display="none"
+    document.getElementById("laptops").style.display="none"
+    var k=true
+    if(k=""){
+        return true
+    }
+    else{
+        return false
+    }
+
+}
+
+function phonesabove20k(){
+    document.getElementById("appleproducts").style.display="none"
+    document.getElementById("laptopcatagory").style.display="none"
+    document.getElementById("carouselExampleAutoplaying").style.display="none"
+    document.getElementById("card").style.display="none"
+    document.getElementById("phones0").style.display="block"
+    document.getElementById("yourlapptopyouruse").style.display="none"
+    document.getElementById("laptops").style.display="none"
+    var k=true
+    if(k=""){
+        return true
+    }
+    else{
+        return false
+    }
+
+}
+function appleproducts(){
+    document.getElementById("appleproducts").style.display="block"
+    document.getElementById("laptopcatagory").style.display="none"
+    document.getElementById("carouselExampleAutoplaying").style.display="none"
+    document.getElementById("card").style.display="none"
+    document.getElementById("phones0").style.display="none"
+    document.getElementById("yourlapptopyouruse").style.display="none"
+    document.getElementById("laptops").style.display="none"
+}
+function laptops(){
+    document.getElementById("appleproducts").style.display="none"
+    document.getElementById("laptopcatagory").style.display="none"
+    document.getElementById("carouselExampleAutoplaying").style.display="none"
+    document.getElementById("card").style.display="none"
+    document.getElementById("phones0").style.display="none"
+    document.getElementById("yourlapptopyouruse").style.display="block"
+    document.getElementById("laptops").style.display="block"
+}
+function gaming(){
+    document.getElementById("appleproducts").style.display="none"
+    document.getElementById("laptopcatagory").style.display="none"
+    document.getElementById("carouselExampleAutoplaying").style.display="none"
+    document.getElementById("card").style.display="none"
+    document.getElementById("phones0").style.display="none"
+    document.getElementById("yourlapptopyouruse").style.display="none"
+    document.getElementById("laptops").style.display="none"
+    document.getElementById("gaming").style.display="block"
+    
+    var k=true
+    if(k=""){
+        return true
+    }
+    else{
+        return false
+    }
+
+} 
+
+function submit(){
+    var a=document.getElementById("mail").value
+    var k=true
+if(a==""){
+    document.getElementById("1").innerHTML="Enter the Email ID <br>"
+    k=false
+}
+ 
+if(k=""){
+    return true
+}
+else{
+    return false
+}
+}
